514f13205998a4366dbb8285a398d07a221a5e09
Agnish1611 <sukla.baruipur2@gmail.com>
2024-08-31 05:25:38 +0530

    Feat: Completed landing page

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -2,6 +2,45 @@ import Navbar from '@/components/Navbar';
 import BgVideo from '../assets/videos/cover-video.mp4';
 import { Button } from '@/components/ui/button';
 import Arrow from '../assets/images/arrow.png';
+import Drone from '../assets/images/drone.png';
+import Radar from '../assets/images/radar.png';
+import Shield from '../assets/images/shield.png';
+import Radar2 from '../assets/images/radar2.png';
+
+const features = [
+    {
+        img: Drone,
+        title: 'Accurate Drone-Bird Classification',
+        desc: 'Utilizes micro-Doppler signatures to reliably distinguish drones from birds, enhancing airspace security.'
+    },
+    {
+        img: Radar,
+        title: 'Advanced Radar Technology',
+        desc: 'Leverages Joint Time-Frequency Analysis for precise identification, even in challenging environments.'
+    },
+    {
+        img: Shield,
+        title: 'Enhanced Situational Awareness',
+        desc: 'Improves safety and surveillance by providing clear, actionable insights through innovative radar detection methods.'
+    },
+]
+
+const innovations = [
+    {
+        title: 'Seamless integrations',
+        desc: 'Combines micro-Doppler radar with advanced signal processing, enabling accurate and reliable differentiation between drones and birds in real-time.',
+        line: true
+    },
+    {
+        title: 'Simplified Detection',
+        desc: 'Converts complex radar data into clear, actionable insights, making it easier for operators to enhance security and make informed decisions.',
+        line: true
+    },
+    {
+        title: 'Intuitive Design',
+        desc: ' Designed for ease of use and adaptability, this system sets a new standard in airspace monitoring, ensuring that advanced technology is both accessible and effective.'
+    },
+]
 
 function LandingPage() {
   return (
@@ -29,6 +68,39 @@ function LandingPage() {
                     </p>
                     
                     <Button className='font-montserrat px-10 py-5'>LEARN MORE</Button>
+                </div>
+            </div>
+        </section>
+        <section className='h-screen w-screen'>
+            <div className="bg-black text-white h-screen flex items-center justify-center p-20 font-montserrat">
+                <div className="flex flex-col md:flex-row">
+                    { features.map((feature, i) => {
+                        return (
+                            <div key={i} className="flex flex-col items-center justify-center gap-4">
+                                <img src={feature.img} alt="Drone" className="w-[500px]" />
+                                <h3 className="text-4xl font-medium text-center  max-w-[250px] mb-5">{feature.title}</h3>
+                                <p className="text-center text-md text-gray-400  max-w-[300px]">{feature.desc}</p>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+        </section>
+        <section className='h-screen w-screen p-10'>
+            <div className="flex items-center justify-around h-full p-10">
+                <div className="text-left basis-1/2 pl-10">
+                    <h1 className="text-7xl max-w-[400px] font-semibold leading-tight font-montserrat text-black mb-5">Innovation that flows</h1>
+                    <img src={Radar2} className='h-[300px]' />
+                </div>
+                <div className="flex flex-col items-center justify-center space-y-6 basis-1/2">
+                    {innovations.map((item, i) => {
+                        return (
+                            <div key={i} className={`${item.line && 'border-b-2 border-black'} py-10 font-montserrat`}>
+                                <div className='text-4xl mb-5 font-medium'>{item.title}</div>
+                                <div className='text-sm'>{item.desc}</div>
+                            </div>
+                        )
+                    })}
                 </div>
             </div>
         </section>
