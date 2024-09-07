35a3eedc0437bb54c6d5bcabc5b999e2dc43f2ca
Agnish1611 <sukla.baruipur2@gmail.com>
2024-09-08 04:01:50 +0530

    Added parameters for objects

--- client/src/pages/FileInput.tsx
+++ client/src/pages/FileInput.tsx
@@ -5,11 +5,25 @@ import { Button } from '@/components/ui/button';
 import Upload from '../assets/images/upload.png';
 import { MdCancel } from "react-icons/md";
 import NoFiles from '../assets/images/nofiles.jpg';
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+interface ObjectData{
+    lat?: number,
+    long?: number,
+    vel?: number
+}
 
 function FileInput() {
     const [files, setFiles] = useState<any[]>([]);
     const [uploadedFiles, setUploadedFiles] = useState<any[]>([]);
-
+    const [fileParams, setFileParams] = useState<Record<number, ObjectData>>({});
+ 
     const { getRootProps, getInputProps } = useDropzone({
         accept: 'image/*,application/pdf', 
         onDrop: (acceptedFiles) => {
@@ -23,6 +37,16 @@ function FileInput() {
         setFiles(updatedFiles);
     };
 
+    const handleParameterChange = (index: number, field: keyof ObjectData, value: number) => {
+        setFileParams((prev) => ({
+          ...prev,
+          [index]: {
+            ...prev[index],
+            [field]: value,
+          },
+        }));
+      };
+
     const handleSubmit = (event) => {
         event.preventDefault();
         const url = 'http://localhost:5000/predict';
@@ -32,13 +56,16 @@ function FileInput() {
             },
         };
 
-        files.forEach((file) => {
+        files.forEach((file, index) => {
             const formData = new FormData();
             formData.append('file', file);
 
             axios.post(url, formData, config)
                 .then((response) => {
-                    setUploadedFiles((prev) => [...prev, response.data.class]);
+                    setUploadedFiles((prev) => [
+                        ...prev,
+                        { class: response.data.class, params: fileParams[index] },
+                    ]);
                 })
                 .catch((err) => {
                     console.error(err);
@@ -48,28 +75,67 @@ function FileInput() {
 
     return (
         <div className="w-[50vw] flex flex-col items-center justify-center pt-10">
+            {files.length ? (<h4 className='text-blue-400 font-semibold font-montserrat'>{files.length} file(s) selected:</h4>) : ''}
             <div className="w-full flex flex-col items-center justify-center">
                 <div 
-                    className={`drag-drop-box ${files.length > 0 ? 'files-selected' : ''}`}
+                    className={`drag-drop-box ${files.length > 0 ? 'files-selected' : ''} overflow-y-scroll`}
                     {...getRootProps()}
                 >
-                    <input {...getInputProps()} />
+                    <input {...getInputProps()} onChange={() => console.log('inputed')} />
                     {files.length > 0 ? (
                         <div className="file-preview">
-                            <h4 className='text-blue-400 font-semibold font-montserrat'>{files.length} file(s) selected:</h4>
                             <ul>
                                 {files.map((file, index) => (
                                     <li key={index} className='font-montserrat flex items-center gap-5 justify-between border border-zinc-600 rounded-lg my-1 px-5'>
                                         {file.name}
-                                        <button 
-                                            className="remove-btn" 
-                                            onClick={(e) => {
-                                                e.stopPropagation();
-                                                handleRemoveFile(index);
-                                            }}
-                                        >
-                                            <MdCancel className='h-5 w-5' />
-                                        </button>
+                                            <Popover>
+                                                <PopoverTrigger asChild>
+                                                    <Button className='text-xs px-2 my-1' onClick={(event) => event.stopPropagation()}>Parameters</Button>
+                                                </PopoverTrigger>
+                                                <PopoverContent className="w-fit z-20" onClick={(event) => event.stopPropagation()}>
+                                                    <div className="grid gap-4">
+                                                    <div className="space-y-2">
+                                                        <h4 className="font-medium leading-none">Parameters</h4>
+                                                        <p className="text-sm text-muted-foreground">
+                                                        Set the parameters for the object.
+                                                        </p>
+                                                    </div>
+                                                    <div className="grid gap-2">
+                                                        <div className="grid grid-cols-3 items-center gap-4">
+                                                        <Label htmlFor="width">Latitude</Label>
+                                                        <Input
+                                                            type='number'
+                                                            id="width"
+                                                            defaultValue="41.40338"
+                                                            className="col-span-2 h-8"
+                                                            onChange={(e) => handleParameterChange(index, 'lat', parseFloat(e.target.value))}
+                                                        />
+                                                        </div>
+                                                        <div className="grid grid-cols-3 items-center gap-4">
+                                                        <Label htmlFor="maxWidth">Longitude</Label>
+                                                        <Input
+                                                            type='number'
+                                                            id="maxWidth"
+                                                            defaultValue="2.17403"
+                                                            className="col-span-2 h-8"
+                                                            onChange={(e) => handleParameterChange(index, 'long', parseFloat(e.target.value))}
+                                                        />
+                                                        </div>
+                                                        <div className="grid grid-cols-3 items-center gap-4">
+                                                        <Label htmlFor="height">Velocity</Label>
+                                                        <Input
+                                                            type='number'
+                                                            id="height"
+                                                            defaultValue="25"
+                                                            className="col-span-2 h-8"
+                                                            onChange={(e) => handleParameterChange(index, 'vel', parseFloat(e.target.value))}
+                                                        />
+                                                        </div>
+                                                    </div>
+                                                    </div>
+                                                </PopoverContent>
+                                            </Popover>
+                                            {/* <MdCancel className='h-5 w-5' /> */}
                                     </li>
                                 ))}
                             </ul>
@@ -90,20 +156,30 @@ function FileInput() {
                 </div>
                 <Button className='m-10 px-7 py-5 text-md' onClick={handleSubmit}>View Results</Button>
             </div>
-            <div className="w-[50vw] h-screen">
+            <div className="w-[50vw] h-fit">
                 {uploadedFiles.length 
                     ? (
-                        <>
-                            <h3>Uploaded Files:</h3>
+                        <div className='flex flex-col justify-center items-center'>
+                            <h3 className='font-montserrat text-lg font-bold'>Objects Detected:</h3>
                             <ul id="result">
                                 {uploadedFiles.map((file, index) => (
                                     <li key={index}>
-                                        {files[index].name}
-                                        {file}
+                                        <div>
+                                        <strong>Class:</strong> {file.class}
+                                        </div>
+                                        <div>
+                                        <strong>Latitude:</strong> {file.params?.lat}
+                                        </div>
+                                        <div>
+                                        <strong>Longitude:</strong> {file.params?.long}
+                                        </div>
+                                        <div>
+                                        <strong>Velocity:</strong> {file.params?.vel}
+                                        </div>
                                     </li>
                                 ))}
                             </ul>
-                        </>
+                        </div>
                     )
                     : (
                         <div className='flex flex-col justify-start items-center h-full w-full'>
